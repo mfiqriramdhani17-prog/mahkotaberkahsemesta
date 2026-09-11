288c15eeb540b4df0217ae5aac5fc7894c3455e0
Mahkota Berkah Semesta <mfiqri.ramdhani17@gmail.com>
2026-09-11 22:57:56 +0700

    Update data.js

--- data.js
+++ data.js
@@ -322,7 +322,7 @@ const PRODUCTS = [
   nama: "Jaket Hoodie Promosi Custom",
   kategori: "jaket",
   foto: [
-    { url: ""assets/img/katalog/26_NavyYellowHood.png", label: "Tampak Depan" },
+    { url: ""assets/img/katalog/26_NavyYellowHood.jpg", label: "Tampak Depan" },
     { url: null, label: "Tampak Belakang" },
     { url: null, label: "Detail Sablon" }
   ],
